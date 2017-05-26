48e0f5600d4d0e71d2ecef68d198c5b5059d8cd1
rodionvolovik <rodionvolovik@gmail.com>
2017-05-26 14:55:04 +0300

    findbook

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # findbook
+
+A simple python script to detect books on image and count its quantity.
+
+It outputs and image with a green contour around the book founded and the number of books on console.
+
+#usage
+
+    python find_book.py [image1] [image2] ...
---
+++ find_books.py
@@ -0,0 +1,34 @@
+# import of necessary package
+
+import sys
+
+import numpy as np
+import cv2
+
+image_name = sys.argv[0]
+print(image_name)
+image = cv2.imread(image_name)
+gray_im = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray_im = cv2.GaussianBlur(gray_im, (3, 3), 0)
+cv2.imwrite('gray.jpg', gray_im);
+
+edged = cv2.Canny(gray_im, 10, 250)
+cv2.imwrite("edged.jpg", edged)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+closed = (cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel));
+cv2.imwrite("closed.jpg", closed);
+
+cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
+total = 0
+
+for c in cnts:
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+	if len(approx) == 4:
+		cv2.drawContours(image, [approx], -1, (0, 255, 0), 4)
+		total += 1
+
+output = cv2.imwrite("output.jpg", image)
+print("I found {0} books").format(total)
